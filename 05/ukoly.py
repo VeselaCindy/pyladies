dde945bef8ba4360c9289ec4a3ebb3be5a764328
VeselaCindy <cindy.v@seznam.cz>
2020-06-18 19:33:37 +0200

    pep8

--- 05/ukoly.py
+++ 05/ukoly.py
@@ -1,15 +1,16 @@
-
 # 0
-zvirata = ['pes', 'kocka','andulka','králík', 'had', 'pav']
+zvirata = ['pes', 'kocka', 'andulka', 'králík', 'had', 'pav']
 
-"""
+
 # 1
 def print_names_less_5_letter(seznam):
     for i in seznam:
         if len(i) < 5:
             print(i)
 
+
 print_names_less_5_letter(zvirata)
+
 
 # 2
 def print_names_start_with_k(seznam):
@@ -17,12 +18,15 @@ def print_names_start_with_k(seznam):
         if i[0] == 'k':
             print(i)
 
+
 # 3
 
 def word_in_list(slovo, seznam):
     print(slovo in seznam)
 
+
 word_in_list('pes', zvirata)
+
 
 # 4
 def make_lists(list1, list2):
@@ -31,19 +35,18 @@ def make_lists(list1, list2):
     prvni = []
     druhy = []
     for i in list1:
-        if not i in list2:
+        if i not in list2:
             prvni.append(i)
 
     for i in list2:
-        if not i in list1:
+        if i not in list1:
             druhy.append(i)
 
     for i in list1:
         if i in list2:
             prunik.append(i)
-    
     for i in list2:
-        if not i in sjednoceni:
+        if i not in sjednoceni:
             sjednoceni.append(i)
 
     print('pouze v prvnim: ', prvni)
@@ -52,9 +55,9 @@ def make_lists(list1, list2):
     print('v obou', prunik)
     return prvni, druhy, sjednoceni
 
-make_lists(['jedna', 'dva','tri'], ['jedna', 'ahoj'])
 
-"""
+make_lists(['jedna', 'dva', 'tri'], ['jedna', 'ahoj'])
+
 
 # 5
 
@@ -62,50 +65,50 @@ def sort_animals(list1):
     serad = sorted(list1)
     print(serad)
 
+
 # sort_animals(zvirata)
 
 # 6
-def sort_accort_second_letter(list1):
-    podle_klice  = dict([])
+def sort_accord_second_letter(list1):
+    podle_klice = dict([])
     for zvire in list1:
         klic = zvire[1]
-        podle_klice.update({klic : zvire})
+        podle_klice.update({klic: zvire})
 
     seradene = []
-    for key, value in sorted (podle_klice.items()) :  
+    for key, value in sorted(podle_klice.items()):
         seradene.append(value)
     print(seradene)
     return seradene
 
-# sort_accort_second_letter(zvirata)
+
+# sort_accord_second_letter(zvirata)
 
 # 7
-# rodne cislo 
-def kontrola_rodne_cislo():
-    rodne_cislo = input('Zadej rodne cislo: ')
-    if not '/' in rodne_cislo or len(rodne_cislo) != 11:
-        raise Exception("Nespravny format")
+# rodne cislo
+def check_rc():
+    rc = input('Zadej rodne cislo: ')
+    if '/' not in rc or len(rc) != 11:
+        raise Exception("Wrong format")
     else:
         # rodne_cislo = rodne_cislo.split('/')
         print('ok')
 
     cislo = ''
-    for i, char in enumerate(rodne_cislo):
+    for i, char in enumerate(rc):
         if i == 6:
             if char != '/':
                 return False
         else:
             if not char.isnumeric():
-               return False
+                return False
             cislo += char
 
-    if int(cislo)%11 == 0:
+    if int(cislo) % 11 == 0:
         print('Rodne cislo je platne.')
         return True
     else:
         return False
 
-kontrola_rodne_cislo()
 
-    
-
+check_rc()
