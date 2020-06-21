e035a9cdee91b6a167b33d5b3f9e9ab613782898
Cindy Veselá <cindy.v@seznam.cz>
2020-06-21 15:42:13 +0200

    move, move gamer

--- 06/hw_connect3.py
+++ 06/hw_connect3.py
@@ -10,3 +10,29 @@ def evaluate(line):
 
 
 print(evaluate('oxooxxoxx'))
+
+
+def move(line, position, symbol):
+    line = list(line)
+    line[position] = symbol
+    return ''.join(line)
+
+
+def move_gamer(line):
+    while True:
+        try:
+            position = int(input('Input position: ')) - 1
+            if position < len(line) and line[position] == '-':
+                while True:
+                    symbol = input('Symbol: ')
+                    if symbol == 'o' or symbol == 'x':
+                        return move(line, position, symbol)
+                    else:
+                        print('Wrong symbol, input x or o, please...')
+            else:
+                print('Wrong position, input it again, please...')
+        except ValueError:
+            print('Input value, please.. try again..')
+
+
+print(move_gamer('xxxx-'))
